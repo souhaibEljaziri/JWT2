cd5a42f3da5a53f24a9e1e69b1075530b4f939ba
seljaziri <souhaib.aljaziri@gmail.com>
2024-06-06 22:05:21 +0100

    fix: step1

---
+++ react/src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="text-center mb-4">Welcome to Our Website</h1>
+              <p className="lead">
+                This is a simple home page 
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
